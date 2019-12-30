7ddd9d0482fad2361b66d86af403036c069e1204
Kristin Helker <klhelker@gmail.com>
2019-12-30 14:21:46 -0600

    updating unit-4-game/Crystal Game

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+
+
+var randomNums = Math.floor(Math.random() * (120 - 19 + 1)) + 19;;
+document.getElementById("randomNum").innerHTML = randomNums;
+
+function myFunction() {
+  randomNums.push();
+  document.getElementById("randomNum").innerHTML = randomNums;
+}
+
+
+ 
