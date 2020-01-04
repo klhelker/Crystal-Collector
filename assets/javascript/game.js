68691154b58d8886eb13fc7db39d6299be5c224a
Kristin Helker <klhelker@gmail.com>
2020-01-04 09:30:56 -0600

    finishing game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,9 @@
 var wins=0;
 var losses=0;
-
 var counter = 0;
 
 var randomNums = Math.floor(Math.random() * (120 - 19 + 1)) + 19;;
 
-
-
-// Each crystal should have a random hidden value between 1 - 12
-// Here we set the "number-to-guess" header to match the "targetNumber".
-// Eventually this will allow us to change the HTML to match the value in the JavaScript.
 $("#randomNum").text(randomNums);
 
 function reset (){
@@ -31,31 +25,31 @@ function reset (){
 
 $(document).on("click", ".crystal-img", function() {
      
-    console.log("clicking");
+    
 
     var crystalValue = ($(this).attr("data-crystalvalue"));
 
-    console.log(crystalValue);
+    
 
     crystalValue = parseInt(crystalValue);
 
   counter += crystalValue;
-    console.log (counter);
+    
   $("#currentScore").text(counter);
 
   if (counter === randomNums) {
     alert("You win!");
     wins+=1
-    console.log (wins,losses);
-    print new win value to page $()
+    
+    $("#wins").text(wins);
     reset()
   }
 
   else if (counter >= randomNums) {
     alert("You lose!!");
     losses+=1
-    console.log (wins,losses)
-    print new loss value page $()
+    
+    $("#losses").text(losses);
     reset()
   }
 
